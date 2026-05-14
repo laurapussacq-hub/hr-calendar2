831a0dc3e78ad44f00ee9c6bbac765d77277fd85
laurapussacq-hub <laura.pussacq@clinicaboedo.com>
2026-05-14 17:56:41 -0300

    Update airtable.js

--- pages/api/pages/api/events/pages/pages/lib/airtable.js
+++ pages/api/pages/api/events/pages/pages/lib/airtable.js
@@ -1,5 +1,4 @@
-Pegá este código:
-javascriptimport Airtable from 'airtable';
+import Airtable from 'airtable';
 
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   process.env.AIRTABLE_BASE_ID
